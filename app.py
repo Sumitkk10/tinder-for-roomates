f23b08b69614eb9b06d1f7f3d4e125b1eaf51f24
sumitkk10 <sumitkk01010@gmail.com>
2023-05-07 11:36:02 +0530

    Linked ketaki and sumit's pages

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
 
-mail = ""
-password = ""
-
 @app.route('/')
 def hello() -> str:
     return "WELCOME TO ROOMATE FINDER"
+
+mail=""
 
 @app.route('/login')
 def login():
@@ -16,7 +15,7 @@ def login():
 
 @app.route('/loginn', methods = ['POST'])
 def loginn():
-    #do something
+    global mail
     db = sqlite3.connect('temp.db')
     mail = request.form['email']
     password = request.form['password']
@@ -33,7 +32,7 @@ def loginn():
             if(x[0][0] == password):
                 db.close()
                 # redirect to search page
-                return "Email exists"
+                return redirect(url_for('results'))
             else:
                 db.close()
                 error = True
@@ -55,6 +54,7 @@ def signup():
 
 @app.route('/signupp', methods=['POST'])
 def signupp():
+    global mail
     db = sqlite3.connect('temp.db')
     mail = request.form['email']
     password = request.form['password']
@@ -88,11 +88,53 @@ def signupp():
         db.commit()
         db.close()
         return render_template('form.html')
-    
+
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    # do something
+    #print(g.mail)
+    global mail
+    db = sqlite3.connect('temp.db')
+    name = request.form['name']
+    branch = request.form['branch']
+    cg = request.form['cg']
+    gender = request.form.get('gender')
+    age = request.form['age']
+    sleeptime = request.form.get('sleeptime')
+    wakeup = request.form.get('wakeup')
+    organised = request.form.get('organised')
+    social = request.form.get('social')
+    noise = request.form.get('noise')
+    smoke = request.form.get('smoke')
+    medications = request.form.get('medications')
+    cg_p = request.form['cg_p']
+    branch_p = request.form['branch_p']
+    smoke_p = request.form.get('smoke_p')
+
+    cursor = db.cursor()
+    try:
+        cursor.execute('''CREATE TABLE details(email varchar(100) PRIMARY KEY,name varchar(100),branch varchar(100),cg INTEGER,gender varchar(100),age varchar(100),sleeptime varchar(100),wakeup varchar(100),organised INTEGER,social INTEGER,noise INTEGER,smoke INTEGER,medications INTEGER,cg_p INTEGER,branch_p varchar(100),smoke_p INTEGER)''')
+    # # db.close()
+    # # db = sqlite3.connect('temp.db')
+    except sqlite3.OperationalError:
+        # Table already exists
+        pass
+    # cursor = db.cursor()
+    cursor.execute(
+        "INSERT into details(email,name,branch,cg,gender,age,sleeptime,wakeup,organised,social,noise,smoke,medications,cg_p,branch_p,smoke_p) VALUES (?, ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", (mail, name, branch,cg,gender,age,sleeptime,wakeup,organised,social,noise,social,medications,cg_p,branch_p,smoke_p))
+    db.commit()
+    db.close()
+    return "Succesfully submitted"
+
 @app.route('/results')
 def results():
-    return "hello"
-    # go to search page with extracted details
+    
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port="5001")
