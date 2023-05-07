92353b381a7d34a4c500fbc24f9cca90968951ff
Ketaki1906 <ketakishetye2004@gmailcom>
2023-05-07 15:01:09 +0530

    form.html and app.py updated finally with form pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
@@ -15,12 +15,10 @@ def login():
 
 @app.route('/loginn', methods = ['POST'])
 def loginn():
-    #do something
     global mail
     db = sqlite3.connect('temp.db')
     mail = request.form['email']
     password = request.form['password']
-    #print(g.mail)
     cursor = db.cursor()
     cursor.execute("SELECT name FROM sqlite_master WHERE type = 'table' AND name=?", ('members',))
     result = cursor.fetchone()
@@ -34,7 +32,7 @@ def loginn():
             if(x[0][0] == password):
                 db.close()
                 # redirect to search page
-                return "Email exists"
+                return redirect(url_for('results'))
             else:
                 db.close()
                 error = True
@@ -60,7 +58,6 @@ def signupp():
     db = sqlite3.connect('temp.db')
     mail = request.form['email']
     password = request.form['password']
-    #print(g.mail)
     cursor = db.cursor()
     cursor.execute("SELECT name FROM sqlite_master WHERE type = 'table' AND name=?", ('members',))
     result = cursor.fetchone()
@@ -105,6 +102,7 @@ def register():
     global mail
     db = sqlite3.connect('temp.db')
     name = request.form['name']
+    image = request.form['image']
     branch = request.form['branch']
     cg = request.form['cg']
     gender = request.form.get('gender')
@@ -117,12 +115,13 @@ def register():
     smoke = request.form.get('smoke')
     medications = request.form.get('medications')
     cg_p = request.form['cg_p']
+    cg_p_max = request.form['cg_p_max']
     branch_p = request.form['branch_p']
     smoke_p = request.form.get('smoke_p')
 
     cursor = db.cursor()
     try:
-        cursor.execute('''CREATE TABLE details(email varchar(100) PRIMARY KEY,name varchar(100),branch varchar(100),cg INTEGER,gender varchar(100),age varchar(100),sleeptime varchar(100),wakeup varchar(100),organised INTEGER,social INTEGER,noise INTEGER,smoke INTEGER,medications INTEGER,cg_p INTEGER,branch_p varchar(100),smoke_p INTEGER)''')
+        cursor.execute('''CREATE TABLE details(email varchar(100) PRIMARY KEY,name varchar(100),image varchar(100),branch varchar(100),cg INTEGER,gender varchar(100),age varchar(100),sleeptime varchar(100),wakeup varchar(100),organised INTEGER,social varchar(100),noise varchar(100),smoke varchar(100),medications varchar(100),cg_p INTEGER,cg_p_max INTEGER,branch_p varchar(100),smoke_p varchar(100))''')
     # # db.close()
     # # db = sqlite3.connect('temp.db')
     except sqlite3.OperationalError:
@@ -130,11 +129,29 @@ def register():
         pass
     # cursor = db.cursor()
     cursor.execute(
-        "INSERT into details(email,name,branch,cg,gender,age,sleeptime,wakeup,organised,social,noise,smoke,medications,cg_p,branch_p,smoke_p) VALUES (?, ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", (mail, name, branch,cg,gender,age,sleeptime,wakeup,organised,social,noise,social,medications,cg_p,branch_p,smoke_p))
+        "INSERT into details(email,name,image,branch,cg,gender,age,sleeptime,wakeup,organised,social,noise,smoke,medications,cg_p,cg_p_max,branch_p,smoke_p) VALUES (?, ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", (mail, name,image,branch,cg,gender,age,sleeptime,wakeup,organised,social,noise,smoke,medications,cg_p,cg_p_max,branch_p,smoke_p))
     db.commit()
     db.close()
     return "Succesfully submitted"
 
+@app.route('/results')
+def results():
 
+    
+    global mail
+    db = sqlite3.connect('temp.db')
+    cursor = db.cursor()
+    print(mail)
+    cursor.execute("SELECT gender FROM details WHERE email=?", (mail,))
+    gender = cursor.fetchone()[0]
+    cursor.execute("SELECT smoke_p FROM details WHERE smoke_p")
+    smoke_p = cursor.fetchone()[0]
+    # print(gender)
+    cursor.execute("SELECT * FROM details WHERE gender=? AND smoke_p=?", (gender,smoke_p))
+
+    rows = cursor.fetchall()
+    db.close()
+    return render_template('results.html', rows=rows)
 if __name__ == "__main__":
-    app.run(debug=True,port="5001")
+    app.run(debug=True)
+
